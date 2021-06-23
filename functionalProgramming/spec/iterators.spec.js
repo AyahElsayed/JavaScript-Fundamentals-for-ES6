4ce9de636f0539758a262320f9a1011a0f2bb093
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-06-23 02:21:05 +0200

    for of

--- functionalProgramming/spec/iterators.spec.js
+++ functionalProgramming/spec/iterators.spec.js
@@ -32,3 +32,19 @@ describe("iterables", function () {
     });
 
 });
+
+describe("for of", function () {
+
+    it("can work with iteratables at a high level", function () {
+
+        let sum = 0;
+        let numbers = [1, 2, 3, 4];
+
+        for (let n of numbers) {
+            sum += n;
+        }
+
+        expect(sum).toBe(10);
+    });
+
+});
