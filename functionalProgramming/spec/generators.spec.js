e7bc0bbf8653914effaa8be3a4b2a89f4aa3929e
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-06-24 17:12:06 +0200

    putting it together

--- functionalProgramming/spec/generators.spec.js
+++ functionalProgramming/spec/generators.spec.js
@@ -22,4 +22,56 @@ describe("generators", function () {
 
         expect(sum).toBe(15);
     });
+
+
+    it("can be built by implementing Symbol.iterator", function () {
+
+        class Company {
+            constructor() {
+                this.employees = [];
+            }
+
+            addEmployees(...names) {
+                this.employees = this.employees.concat(names);
+            }
+            *[Symbol.iterator]() {
+                for (let e of this.employees) {
+                    //console.log(e);
+                    yield e;
+                }
+            }
+        }
+
+        let filter = function* (items, predicate) {
+            for (let item of items) {
+                //console.log("filter", item);
+                if (predicate(item)) {
+                    yield item;
+                }
+            }
+        }
+
+        let take = function* (items, number) {
+            let count = 0;
+            if (number < 1) return;
+            for (let item of items) {
+                //console.log("take", item);
+                yield item;
+                count += 1;
+                if (count >= number) {
+                    return;
+                }
+            }
+        }
+
+        let count = 0;
+        let company = new Company();
+        company.addEmployees("Tim", "Sue", "Joy", "Tom");
+
+        for (let employee of take(filter(company, e => e[0] == 'T'), 2)) {
+            count += 1;
+        }
+
+        expect(count).toBe(2);
+    });
 });
