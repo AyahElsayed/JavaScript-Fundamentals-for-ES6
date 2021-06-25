03f36701f8be4c267a252ed3c525eac200a44f58
Ayah <eng.aya.elsayed.2020@gmail.com>
2021-06-25 02:33:40 +0200

    array comprehensions

--- built-in objects/spec/array.spec.js
+++ built-in objects/spec/array.spec.js
@@ -63,5 +63,20 @@ describe('Arrays', function() {
 		expect(firstKey).toBe(0);
 	});
 
-	
+	describe('comprehensions', function() {
+		it('should create arrays easily', function() {
+			var ary = [for (i of [1,2,3]) i]; //[1,2,3]
+			expect(ary[2]).toBe(3);
+			var ary2 = [for (i of [1,2,3]) i*i]; //[1,4,9]
+			expect(ary2[2]).toBe(9);
+			var ary3 = [for (i of [1,2,3]) if (i < 3) i]; //[1,2]
+			expect(ary3.length).toBe(2);
+
+			var ary4 = [for (first of ['William', 'John', 'Blake'])
+				for (middle of ['Robert','Andrew','John'])
+				if(first != middle) (first + ' ' + middle + ' Smith')]
+			console.log(ary4);
+			expect(ary4[0]).toBe('William Robert Smith');
+		});
+	});
 })
